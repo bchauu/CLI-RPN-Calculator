4e718f8b6e4699f5278b78928b7521602f53d068
Bryan <chaubryan@hotmail.com>
2022-06-13 18:27:59 +0700

    separated testing files

---
+++ __tests__/rpn.test.js
@@ -0,0 +1,56 @@
+import rpn from '../rpn.js';
+import operandStack from '../stack.js';
+
+describe('handles one operator entered by user', () => {
+    test('Function should remove numbers from stack; computed result should be pushed to stack', () => {
+        const firstInput = 5;
+        const secondInput = 10;
+        operandStack.push(firstInput);
+        operandStack.push(secondInput);
+        let result = operandStack.size();
+        // const firstOperand = operandStack.pop();
+        // const secondOperand = operandStack.pop();
+        const input = '*';
+        rpn.operatorInput(input);
+        expect(operandStack.size()).toEqual(result - 1);
+        expect(operandStack.pop()).toEqual(firstInput*secondInput);
+        // result = operandStack.size();
+        // operandStack.push(firstOperand*secondOperand);
+        // expect(operandStack.size()).toEqual(result + 1);
+    });
+});
+
+describe('handles one operand entered by user', () => {
+    test('Function should push inputted number to stack', () => {
+        const input = 5;
+        const result = operandStack.size();
+        rpn.numberInput(input);
+        // operandStack.push(input);
+        expect(operandStack.size()).toEqual(result + 1);
+    });
+});
+
+describe('handles multiple inputs entered by user', () => {
+    test('When receiving " ", should push each operator or operand to a stack', () => {
+        operandStack.clear();
+        let input = [
+            '1', '0', ' ',
+            '1', '0', '1',
+            ' ', '1', '0', ' '
+          ];
+        rpn.multipleInputs(input);
+        expect(console.log(operandStack));
+        expect(operandStack.size()).toEqual(3);
+
+        input = [
+            '1', '0', ' ',
+            '1', '0', '1', ' ', '+',
+            ' ', '1', '0', ' '
+          ];
+          operandStack.clear();
+          rpn.multipleInputs(input);
+          expect(console.log(operandStack));
+          expect(operandStack.size()).toEqual(2);
+
+    });
+});
